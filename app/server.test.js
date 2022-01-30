0935744d253a1b62da29cdf1389e978b24c83cc4
jFarrow02 <jack.dempsey.farrow@gmail.com>
2022-01-30 14:18:52 -0500

    fixes unit test

--- app/server.test.js
+++ app/server.test.js
@@ -3,6 +3,5 @@ const fs = require('fs')
 
 test('main index.html file exists', () => {
   const filePath = path.join(__dirname, "index.html")
-  // expect(fs.existsSync(filePath)).toBeTruthy();
-  expect(false).toBeTruthy();
+  expect(fs.existsSync(filePath)).toBeTruthy();
 });
